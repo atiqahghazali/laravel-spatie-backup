74ac3aaa916fdc77398d8ab28b88b0817fa89550
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-12-03 12:18:08 +0800

    change class name

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\DropboxDriveServiceProvider::class,
+        App\Providers\DropboxServiceProvider::class,
         /*
          * Package Service Providers...
          */
